60053c658346df1cd32fcb31ef41ea7ae78f502f
mcarvalho <mcarvalho.dev@gmail.com>
2019-09-14 19:53:01 -0300

    Implementação do serviço ans cancela guia

--- src/main/java/com/ans/tisswebservices/config/WebServiceConfig.java
+++ src/main/java/com/ans/tisswebservices/config/WebServiceConfig.java
@@ -88,6 +88,11 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public SimpleWsdl11Definition tissLoteAnexoV3_04_00() {
 		return new SimpleWsdl11Definition(new ClassPathResource("tissLoteAnexoV3_04_00.wsdl"));
 	}	
+	
+	@Bean
+	public SimpleWsdl11Definition tissCancelaGuiaV3_04_00() {
+		return new SimpleWsdl11Definition(new ClassPathResource("tissCancelaGuiaV3_04_00.wsdl"));
+	}	
 
 	@Bean
 	public XsdSchema tissWebServicesV3_04_00() {
