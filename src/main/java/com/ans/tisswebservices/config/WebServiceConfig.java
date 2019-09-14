d90787a82c48ffb50b41e379d5155c7dc72333fa
mcarvalho <mcarvalho.dev@gmail.com>
2019-09-14 17:44:59 -0300

    Implementação do serviço situacao recurso glosa

--- src/main/java/com/ans/tisswebservices/config/WebServiceConfig.java
+++ src/main/java/com/ans/tisswebservices/config/WebServiceConfig.java
@@ -52,6 +52,11 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public SimpleWsdl11Definition tissSolicitacaoDemonstrativoRetornoV3_04_00() {
 		return new SimpleWsdl11Definition(new ClassPathResource("tissSolicitacaoDemonstrativoRetornoV3_04_00.wsdl"));
 	}
+	
+	@Bean
+	public SimpleWsdl11Definition tissSolicitacaoStatusRecursoGlosaV3_04_00() {
+		return new SimpleWsdl11Definition(new ClassPathResource("tissSolicitacaoStatusRecursoGlosaV3_04_00.wsdl"));
+	}
 
 	@Bean
 	public XsdSchema tissWebServicesV3_04_00() {
